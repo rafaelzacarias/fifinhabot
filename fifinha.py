86586d6fb48ca37d82ce53263dda05ccb17b87d6
Rafael Zacarias <rafael.zacarias@corp.globoi.com>
2018-02-09 17:49:41 -0200

    Adds /update command

--- fifinha.py
+++ fifinha.py
@@ -22,6 +22,7 @@ import logging
 import os
 import random
 import csv
+import sys
 from collections import defaultdict
 
 # Enable logging
@@ -37,6 +38,12 @@ logger = logging.getLogger(__name__)
 def start(bot, update):
     """Send a message when the command /start is issued."""
     update.message.reply_text('COEEEEH RAPAZIAAAAAADA!')
+
+def update(bot, update):
+    update.message.reply_text('reiniciando...')
+    os.system("git pull")
+    update.message.reply_text('sincronizado!')
+    sys.exit()
 
 def help(bot, update):
     """Send a message when the command /help is issued."""
@@ -128,6 +135,7 @@ def main():
     dp.add_handler(CommandHandler("times", pick_teams))
     dp.add_handler(CommandHandler("selecoes_masculinas", pick_men_nationals))
     dp.add_handler(CommandHandler("selecoes_femininas", pick_women_nationals))
+    dp.add_handler(CommandHandler("update", update))
 
     # log all errors
     dp.add_error_handler(error)
---
+++ runbot.py
@@ -0,0 +1,6 @@
+import os, time
+
+while 1:
+    os.system("python fifinha.py")
+    print "Restarting..."
+    time.sleep(0.2) 
