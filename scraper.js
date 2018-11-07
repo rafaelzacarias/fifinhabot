c63e2690be103e644d121431e070122642bfc91e
Lucas Costa <lucasrodcosta@gmail.com>
2018-11-07 16:30:17 -0200

    Update scraper and teams list

--- scraper.js
+++ scraper.js
@@ -1,10 +1,14 @@
-// Run the following script in webpage https://www.fifaindex.com/teams,
+// Run the following script in webpages [1], [2] and [3],
 // copy the results and save in the file "teams.csv"
+//
+// [1] https://www.fifaindex.com/teams
+// [2] https://www.fifaindex.com/teams/?type=1
+// [3] https://www.fifaindex.com/teams/?type=2
 
-$('table.teams tbody tr').each(function(i, e) {
+$('table.table-teams tbody tr').each(function(i, e) {
   var name = $(e).find('td[data-title="Name"]>a:first').text();
-  var star = $(e).find('td[data-title="Team Rating"]>span>i.fa-star').length;
-  var halfStar = $(e).find('td[data-title="Team Rating"]>span>i.fa-star-half-o').length;
+  var star = $(e).find('td[data-title="Team Rating"]>span>i.fas.fa-star').length;
+  var halfStar = $(e).find('td[data-title="Team Rating"]>span>i.fas.fa-star-half-alt').length;
   var rating = star + 0.5 * halfStar;
   console.log(name + ',' + rating);
 });
